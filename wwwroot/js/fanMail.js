915e1bb394adcf6217e991487c4e3728801f5e2c
alexbennett98 <61519320+alexbennett98@users.noreply.github.com>
2021-01-20 20:02:08 -0700

    added wwwroot

---
+++ wwwroot/js/fanMail.js
@@ -0,0 +1,40 @@
+﻿//Define button variable for click event
+var calculate = document.getElementById("submitButton");
+
+//Submit button event
+calculate.addEventListener("click", function () {
+    //Define variables & get input
+    var fAssignments = parseFloat(document.getElementById("assignments").value);
+    var fGroupProject = parseFloat(document.getElementById("group project").value);
+    var fQuizzes = parseFloat(document.getElementById("quizzes").value);
+    var fExams = parseFloat(document.getElementById("exams").value);
+    var fIntex = parseFloat(document.getElementById("intex").value);
+    var fFinalScore;
+    var sLetterGrade = "";
+
+    fFinalScore = fAssignments * .5 + fGroupProject * .1 + fQuizzes * .1 + fExams * .2 + fIntex * .1;
+    if (fFinalScore >= .94)
+        sLetterGrade = "A";
+    else if (fFinalScore >= .9)
+        sLetterGrade = "A-";
+    else if (fFinalScore >= .87)
+        sLetterGrade = "B+";
+    else if (fFinalScore >= .84)
+        sLetterGrade = "B";
+    else if (fFinalScore >= .8)
+        sLetterGrade = "B-";
+    else if (fFinalScore >= .77)
+        sLetterGrade = "C+";
+    else if (fFinalScore >= .74)
+        sLetterGrade = "C";
+    else if (fFinalScore >= .7)
+        sLetterGrade = "C-";
+    else if (fFinalScore >= .67)
+        sLetterGrade = "D+";
+    else if (fFinalScore >= .64)
+        sLetterGrade = "D";
+    else
+        sLetterGrade = "D-";
+    fFinalScore = fFinalScore * 100;
+    alert("Your final score is " + fFinalScore.toString() + " and your letter grade is " + sLetterGrade);
+});
